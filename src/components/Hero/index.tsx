6b91acbb3eae0d2814dd1998dd5de1153d304072
allen <allenmuhani@gmail.com>
2025-03-10 15:50:32 +0300

    cleaned up home page

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import animationData from "../../../public/lottie_animations/painter_at_work.json";
 
 export default function Hero() {
-  const lottieContainer = useRef(null);
 
   useEffect(() => {
 
@@ -17,23 +16,17 @@ export default function Hero() {
       <div className="container mx-auto px-6 lg:px-20 flex flex-col lg:flex-row items-center justify-center justify-around">
         {/* Left Content */}
         <div className="text-center lg:text-left max-w-xl">
-          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 text-center">
-            Colour your world with Next Paints
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
+            Colour your world with <span className="text-4xl md:text-5xl font-bold text-red-700">Next Paints</span>
           </h1>
-          <p className="mt-4 text-lg text-gray-600 text-center">
+          <p className="mt-4 text-lg text-gray-600">
             We provide top-quality solutions tailored to your needs. Join us and take your business to the next level.
           </p>
-          <div className="mt-6 flex justify-center space-around flex-col space-y-4 items-center">
+          <div className="mt-6 flex justify-center space-around flex-col space-y-4">
 
             <Link href="/order">
               <button className="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg hover:bg-red-700 transition">
                 Order Now
-              </button>
-            </Link>
-
-            <Link href="/about">
-              <button className="px-6 py-3 border border-dark-blue-900 text-dark-blue-900 font-semibold rounded-lg hover:bg-gray-900 hover:text-white transition">
-                About Us
               </button>
             </Link>
           </div>
