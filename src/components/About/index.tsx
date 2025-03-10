844338c5827659990319e501800b0021d20f3234
allen <allenmuhani@gmail.com>
2025-03-10 12:08:48 +0300

    added to dos

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,9 +5,10 @@ import animationData from "../../../public/lottie_animations/painter_at_work.jso
 
 export default function AboutUs() {
     return (
-        <section className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto p-6">
+        <section className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto ">
             {/* Left: Lottie Animation */}
             <div className="w-full md:w-1/2 flex justify-center">
+                {/* TODO: Update animation */}
                 <Lottie animationData={animationData} loop={true} className="w-150 h-130" />
             </div>
 
@@ -15,6 +16,7 @@ export default function AboutUs() {
             <div className="w-full md:w-1/2 space-y-4 text-center md:text-left">
                 <h2 className="text-3xl font-bold text-gray-800">About Us</h2>
                 <p className="text-gray-600">
+                    {/* TODO: Update text */}
                     We are a passionate team dedicated to providing top-notch services in
                     our industry. Our mission is to deliver excellence through innovation
                     and commitment.
@@ -29,12 +31,11 @@ export default function AboutUs() {
                 </ul>
 
                 {/* Read More Button */}
-                <a
-                    href="/about"
-                    className="inline-block mt-4 bg-blue-600 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300"
+                <button
+                    className="inline-block mt-4 bg-red-600 text-white px-6 py-2 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300"
                 >
                     Read More
-                </a>
+                </button>
             </div>
         </section>
     );
