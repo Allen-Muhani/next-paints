cec88f4ac014c25a8b24d115f634aa863e2fa426
allen <allenmuhani@gmail.com>
2025-03-24 16:26:52 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enables static export
+  images: { unoptimized: true }, // GitHub Pages does not support Next.js image optimization
+  basePath: "/next-paints", // Replace with your repo name
+  assetPrefix: "/next-paints/",
 };
 
 export default nextConfig;
