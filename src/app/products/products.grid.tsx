d776c704cfae39c9e2d6342785b56fcf898e5a68
jamie-codez <cruiseomondi90@gmail.com>
2025-04-01 10:00:22 +0300

    fix:fixed image uri

--- src/app/products/products.grid.tsx
+++ src/app/products/products.grid.tsx
@@ -10,7 +10,7 @@ const ProductGrid = () => {
             {/* <h2 className="text-2xl font-bold text-center mb-6">Our Products</h2> */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {products.map((product, index) => (
-                    <ProductCard product={product} key={index} ></ProductCard>
+                    <ProductCard product={product} key={index} />
                 ))}
             </div>
         </div>
